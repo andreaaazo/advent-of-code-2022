cf6e6495bce95e57f6f69e1048d0ca13a4a1ee77
Andrea <zorzi.andrea@outlook.com>
2022-12-26 04:41:28 +0100

    Added day_2 question

---
+++ day_2/day_2.py
@@ -0,0 +1,21 @@
+# A = Rock
+# B = Paper
+# C = Scissors
+
+# X = Rock
+# Y = Paper
+# Z = Scissors
+
+
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+class RockPaperScissors:
+    def response(self) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    RockPaperScissors.response()
