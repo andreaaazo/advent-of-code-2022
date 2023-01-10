da643070b42c0921172c5428de23ccf444b1094c
Andrea <zorzi.andrea@outlook.com>
2023-01-10 18:53:52 +0100

    Initialized day 7

---
+++ day_7/day_7.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Response to day 7 of Advent Of Code 2022"""
+
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+class SpaceChecker:
+    def response_part_1(self) -> int:
+        """Response to part 1"""
+        pass
+        return 0
+
+    def response_part_2(self) -> int:
+        """Response to part 2"""
+        pass
+        return 0
+
+
+if __name__ == "__main__":
+    print(SpaceChecker().response_part_1())
+    print(SpaceChecker().response_part_2())
