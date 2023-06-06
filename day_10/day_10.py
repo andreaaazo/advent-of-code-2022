7fe8d6dcbbc46a0010b33f7b469c64f4a0723ff9
Andrea <zorzi.andrea@outlook.com>
2023-06-06 16:07:41 +0200

    Added part 2 day 10

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -11,10 +11,18 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 
 
-def response_part_1():
+def get_input():
     with open(f"{BASE_DIR}/input.txt", "r") as inp:
-        signals = [line.split(" ") for line in inp.read().splitlines()]
+        input = [line.split(" ") for line in inp.read().splitlines()]
         inp.close()
+    return input
+
+
+def response_part_1() -> int:
+    """
+    Response to part 1
+    """
+    signals = get_input()
 
     X = 1
     cycles_counter = 0
@@ -39,5 +47,63 @@ def response_part_1():
     return cycles_value
 
 
+def response_part_2() -> int:
+    signals = get_input()
+
+    X = 1
+    CRT_display = ""
+    CRT_drawing_position = 0
+    sprite_positions = [X - 1, X, X + 1]
+    cycles_counter = 0
+
+    for signal in signals:
+        if len(signal) == 1:  # is noop
+            cycles_counter += 1
+            if (cycles_counter - 1) in [40, 80, 120, 160, 200, 240]:
+                CRT_display += "\n"
+                CRT_drawing_position = 0
+
+            if CRT_drawing_position in sprite_positions:
+                CRT_display += "#"
+                CRT_drawing_position += 1
+            else:
+                CRT_display += "."
+                CRT_drawing_position += 1
+
+        else:  # is addx
+            # 1st loop
+            cycles_counter += 1
+            if (cycles_counter - 1) in [40, 80, 120, 160, 200, 240]:
+                CRT_display += "\n"
+                CRT_drawing_position = 0
+            if CRT_drawing_position in sprite_positions:
+                CRT_display += "#"
+                CRT_drawing_position += 1
+            else:
+                CRT_display += "."
+                CRT_drawing_position += 1
+
+            # 2nd loop
+            cycles_counter += 1
+            if (cycles_counter - 1) in [40, 80, 120, 160, 200, 240]:
+                CRT_display += "\n"
+                CRT_drawing_position = 0
+
+            if CRT_drawing_position in sprite_positions:
+                CRT_display += "#"
+                CRT_drawing_position += 1
+            else:
+                CRT_display += "."
+                CRT_drawing_position += 1
+
+            # end addx
+            X += int(signal[1])
+            sprite_positions = [X - 1, X, X + 1]
+            print((sprite_positions, CRT_drawing_position))
+
+    return CRT_display
+
+
 if __name__ == "__main__":
     print(response_part_1())  # 14560
+    print(response_part_2())  # EKRHEPUZ
